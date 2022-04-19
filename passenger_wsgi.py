8fdb8e3985afc61cb1510829eb08686f473661f9
jordan <jordan@METTULUS-PC>
2022-04-19 16:56:59 -0700

    test 3

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,21 +1,11 @@
 import sys, os
 
-from artishan.app import appmain
 INTERP = os.path.join(os.environ['HOME'], 'artishan.io', 'venv', 'bin', 'python3')
 if sys.executable != INTERP:
         os.execl(INTERP, INTERP, *sys.argv)
 sys.path.append(os.getcwd())
 
-from flask import Flask
-from app import app
-#from app.appmain import application as application
+#from flask import Flask
 
-application = app
-
-#application = Flask(__name__)
-
-@application.route('/index')
-@application.route('/home')
-@application.route('/')
-def index():
-    return 'Passenger WSGI'
+sys.path.append('app')
+from app import app as application
