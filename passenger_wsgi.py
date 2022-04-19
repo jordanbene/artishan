43874c6415131843f931a3f017c7f293d74a358c
jordan <jordan@METTULUS-PC>
2022-04-19 16:12:40 -0700

    test

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -5,9 +5,9 @@ if sys.executable != INTERP:
 sys.path.append(os.getcwd())
 
 from flask import Flask
-from app import appmain as application
+#from app import appmain as application
 
-#application = Flask(__name__)
+application = Flask(__name__)
 
 @application.route('/index')
 @application.route('/home')
