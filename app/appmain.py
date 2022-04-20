f0345f7bc4e739ca61b85e68d551c8ab17e92420
jordan <jordan@METTULUS-PC>
2022-04-19 20:55:26 -0700

    debug 3

--- app/appmain.py
+++ app/appmain.py
@@ -1,18 +1,19 @@
 from flask.templating import Flask, render_template
+from werkzeug.exceptions import HTTPException
 from app import app as app
 
 @app.route('/')
-@app.route('/index')
 def index():
     print("success")
     #return 'appmain'
 
     return render_template('index.html', title="Test")
 
-@app.errorhandler(404)
-def pageNotFound(error):
-    return "page not found", 404
+@app.errorhandler(Exception)
+def handle_exception(e):
+    # pass through HTTP errors. You wouldn't want to handle these generically.
+    if isinstance(e, HTTPException):
+        return e
 
-@app.errorhandler(500)
-def special_exception_handler(error):
-    return "Database connection failed", 500
+    # now you're handling non-HTTP exceptions only
+    return render_template("500_generic.html", e=e), 500
--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,5 @@ from flask.templating import Flask, render_template
 from app import app
 
 @app.route('/')
-@app.route('/index')
-@app.route('/home')
 def index():
     return render_template('index.html', title="Test")
