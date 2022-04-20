d627b847a14535178b2f13b52c0c82de74b0440f
jordan <jordan@METTULUS-PC>
2022-04-19 21:04:18 -0700

    methods post get

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask.templating import Flask, render_template
 from app import app
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html', title="Test")
