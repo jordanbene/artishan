2affed6faff6c2edc7f6b573ee70c9dee7717e8e
jordan <jordan@METTULUS-PC>
2022-04-19 20:07:55 -0700

    ddd

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,4 +1,6 @@
 import sys, os
+from flask.templating import Flask, render_template
+
 
 INTERP = os.path.join(os.environ['HOME'], 'artishan.io', 'venv', 'bin', 'python3')
 if sys.executable != INTERP:
