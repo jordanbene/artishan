eead35f3a5cd9a91b4741b3691c4d4a12f6971cf
jordan <jordan@METTULUS-PC>
2022-04-20 18:06:38 -0700

    dd

--- app/glidemodel.py
+++ app/glidemodel.py
@@ -12,11 +12,11 @@ from glide_text2im.model_creation import (
 # This notebook supports both CPU and GPU.
 # On CPU, generating one sample may take on the order of 20 minutes.
 # On a GPU, it should be under a minute.
- has_cuda = th.cuda.is_available()
- device = th.device('cpu' if not has_cuda else 'cuda')
+has_cuda = th.cuda.is_available()
+device = th.device('cpu' if not has_cuda else 'cuda')
 options = model_and_diffusion_defaults()
 options_up = model_and_diffusion_defaults_upsampler()
-
+samples
 def createmodel():
    
     # Create base model.
@@ -29,60 +29,53 @@ def createmodel():
     model.to(device)
     model.load_state_dict(load_checkpoint('base', device))
     print('total base parameters', sum(x.numel() for x in model.parameters()))
+    return model
 
-# Create upsampler model.
-options_up['use_fp16'] = has_cuda
-options_up['timestep_respacing'] = 'fast27' # use 27 diffusion steps for very fast sampling
-model_up, diffusion_up = create_model_and_diffusion(**options_up)
-model_up.eval()
-if has_cuda:
-    model_up.convert_to_fp16()
-model_up.to(device)
-model_up.load_state_dict(load_checkpoint('upsample', device))
-print('total upsampler parameters', sum(x.numel() for x in model_up.parameters()))
 
 def show_images(batch: th.Tensor):
     """ Display a batch of images inline. """
     scaled = ((batch + 1)*127.5).round().clamp(0,255).to(th.uint8).cpu()
     reshaped = scaled.permute(2, 0, 3, 1).reshape([batch.shape[2], -1, 3])
-    display(Image.fromarray(reshaped.numpy()))
+    display(Image.fromarray(reshaped.numpy())) #change to json image?
+def requestimage():
+    # Sampling parameters
+    prompt = "an oil painting of a corgi"
+    batch_size = 1
+    guidance_scale = 3.0
 
-# Sampling parameters
-prompt = "an oil painting of a corgi"
-batch_size = 1
-guidance_scale = 3.0
+    # Tune this parameter to control the sharpness of 256x256 images.
+    # A value of 1.0 is sharper, but sometimes results in grainy artifacts.
+    upsample_temp = 0.1
 
-# Tune this parameter to control the sharpness of 256x256 images.
-# A value of 1.0 is sharper, but sometimes results in grainy artifacts.
-upsample_temp = 0.997
+    ##############################
+    # Sample from the base model #
+    ##############################
 
-##############################
-# Sample from the base model #
-##############################
+    # Create the text tokens to feed to the model.
+    tokens = model.tokenizer.encode(prompt)
+    tokens, mask = model.tokenizer.padded_tokens_and_mask(
+        tokens, options['text_ctx']
+    )
 
-# Create the text tokens to feed to the model.
-tokens = model.tokenizer.encode(prompt)
-tokens, mask = model.tokenizer.padded_tokens_and_mask(
-    tokens, options['text_ctx']
-)
+    # Create the classifier-free guidance tokens (empty)
+    full_batch_size = batch_size * 2
+    uncond_tokens, uncond_mask = model.tokenizer.padded_tokens_and_mask(
+        [], options['text_ctx']
+    )
 
-# Create the classifier-free guidance tokens (empty)
-full_batch_size = batch_size * 2
-uncond_tokens, uncond_mask = model.tokenizer.padded_tokens_and_mask(
-    [], options['text_ctx']
-)
+    # Pack the tokens together into model kwargs.
+    model_kwargs = dict(
+        tokens=th.tensor(
+            [tokens] * batch_size + [uncond_tokens] * batch_size, device=device
+        ),
+        mask=th.tensor(
+            [mask] * batch_size + [uncond_mask] * batch_size,
+            dtype=th.bool,
+            device=device,
+        ),
+    )
 
-# Pack the tokens together into model kwargs.
-model_kwargs = dict(
-    tokens=th.tensor(
-        [tokens] * batch_size + [uncond_tokens] * batch_size, device=device
-    ),
-    mask=th.tensor(
-        [mask] * batch_size + [uncond_mask] * batch_size,
-        dtype=th.bool,
-        device=device,
-    ),
-)
+    sample_model()
 
 # Create a classifier-free guidance sampling function
 def model_fn(x_t, ts, **kwargs):
@@ -96,17 +89,18 @@ def model_fn(x_t, ts, **kwargs):
     return th.cat([eps, rest], dim=1)
 
 # Sample from the base model.
-model.del_cache()
-samples = diffusion.p_sample_loop(
-    model_fn,
-    (full_batch_size, 3, options["image_size"], options["image_size"]),
-    device=device,
-    clip_denoised=True,
-    progress=True,
-    model_kwargs=model_kwargs,
-    cond_fn=None,
-)[:batch_size]
-model.del_cache()
+def sample_model():
+    model.del_cache()
+    global samples = diffusion.p_sample_loop(
+        model_fn,
+        (full_batch_size, 3, options["image_size"], options["image_size"]),
+        device=device,
+        clip_denoised=True,
+        progress=True,
+        model_kwargs=model_kwargs,
+        cond_fn=None,
+    )[:batch_size]
+    model.del_cache()
 
 # Show the output
 def showimages():
