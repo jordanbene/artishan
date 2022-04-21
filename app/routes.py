bf19ff7e2715a66b74f463d148ec698e93e68e82
jordan <jordan@METTULUS-PC>
2022-04-21 12:17:55 -0700

    dw

--- app/routes.py
+++ app/routes.py
@@ -5,6 +5,6 @@ from app import app
 @app.route('/', methods=['GET', 'POST'])
 def index():
     #print("routes accessed")
-    #return render_template('index.html', title="Test Title From Routes")
-    return print("routes accessed")
+    return render_template('index.html', title="Test Title From Routes")
+    #return print("routes accessed")
 
