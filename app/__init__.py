f30e4f2e7132a5cefc7d190db85d675a780a6929
jordan <jordan@METTULUS-PC>
2022-04-21 12:02:28 -0700

    added: templates folder

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
-app = Flask(__name__, template_folder="static", static_folder="static")
+app = Flask(__name__, template_folder="templates", static_folder="static")
 
 from app import routes
 app.debug = False
