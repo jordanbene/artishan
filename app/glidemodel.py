3b443f06e0ad000d78df938ba7eea9330e412c64
jordan <jordan@METTULUS-PC>
2022-04-24 12:08:43 -0700

    d

--- app/glidemodel.py
+++ app/glidemodel.py
@@ -26,7 +26,7 @@ options = model_and_diffusion_defaults()
 options['use_fp16'] = has_cuda
 options['timestep_respacing'] = '25' # use 100 diffusion steps for fast sampling
 #model, diffusion = create_model_and_diffusion(**options)
-model, diffusion = th.load(PATH)
+model, diffusion = dataiter.next(th.load(PATH)) 
 model.eval()
 if has_cuda:
     model.convert_to_fp16()
