27bc058fcab63a62abd348b2f9eca0ecae946ec9
jordan <jordan@METTULUS-PC>
2022-04-25 15:11:29 -0700

    d

--- app/glidemodel.py
+++ app/glidemodel.py
@@ -39,7 +39,7 @@ if has_cuda:
     model.convert_to_fp16()
 model.to(device)
 #model.load_state_dict(load_checkpoint('localbase', device))
-model.load_state_dict(checkpoint['text2im'], device)
+model.load_state_dict(checkpoint['base'], device)
 
 print('total base parameters', sum(x.numel() for x in model.parameters()))
 
