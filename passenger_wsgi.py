0a83b06eabf2ddcc66fa2487655b778de2c0a35a
jordan <jordan@METTULUS-PC>
2022-04-29 14:41:02 -0700

    d

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,4 +1,3 @@
-#!/home/dh_nfjxcr/artishan.io/venv/bin/python3
 import sys, os
 
 INTERP = os.path.join(os.environ['HOME'], 'artishan.io', 'venv', 'bin', 'python3')
@@ -8,8 +7,7 @@ sys.path.append(os.getcwd())
 
 #sys.path.append('home/artishan.io/myapp')
 sys.path.append(os.path.join(os.environ['HOME'], 'artishan.io', 'myapp'))
-
-#sys.path.append('myapp')
+sys.path.append('myapp')
 from myapp import app as application
  
 x = open(os.path.expanduser('~/artishan.io/log.log'), 'w')
