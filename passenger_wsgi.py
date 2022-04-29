f43cc199796bb4d8f45d85b94572ea8165269c26
jordan <jordan@METTULUS-PC>
2022-04-29 14:23:48 -0700

    d

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -6,9 +6,10 @@ if sys.executable != INTERP:
         os.execl(INTERP, INTERP, *sys.argv)
 sys.path.append(os.getcwd())
 
-#sys.path.append('home/artishan.io/app')
+#sys.path.append('home/artishan.io/myapp')
+sys.path.append(os.environ['HOME'], 'artishan.io', 'myapp')
 
-sys.path.append('myapp')
+#sys.path.append('myapp')
 from myapp import app as application
  
 x = open(os.path.expanduser('~/artishan.io/log.log'), 'w')
