87f1cccbd30347fa644331c79be1ded33b51ccf8
jordan <jordan@METTULUS-PC>
2022-04-30 12:38:06 -0700

    d

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,15 +1,20 @@
+#!/home/dh_nfjxcr/opt/python-3.8.2/bin/python3
 import sys, os
 
 INTERP = os.path.join(os.environ['HOME'], 'opt', 'python-3.8.2', 'bin', 'python3')
 #INTERP = os.path.join(os.environ['HOME'], 'artishan.io', 'venv', 'bin', 'python3')
 if sys.executable != INTERP:
-        print("Invalid Path")
+        print("Adding Path")
         os.execl(INTERP, INTERP, *sys.argv)
-sys.path.append(os.getcwd())
+sys.path.append(os.getcwd(), 'myapp')
 
 #sys.path.append('home/artishan.io/myapp')
 sys.path.append(os.path.join(os.environ['HOME'], 'artishan.io', 'myapp'))
-sys.path.append('myapp')
+
+sys.path.insert(0,'$HOME/opt/python-3.8.2/bin/python3')
+sys.path.insert(0,'$HOME/artishan.io/myapp')
+sys.path.insert(0,'$HOME/artishan.io/venv/bin')
+
 from myapp import app as application
  
 x = open(os.path.expanduser('~/artishan.io/log.log'), 'w')
