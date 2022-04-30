a9070f7a5b1e93f1e2b65c782e4e2a32bbe24622
jordan <jordan@METTULUS-PC>
2022-04-30 12:42:18 -0700

    d

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,12 +1,13 @@
 #!/home/dh_nfjxcr/opt/python-3.8.2/bin/python3
 import sys, os
 
-INTERP = os.path.join(os.environ['HOME'], 'opt', 'python-3.8.2', 'bin', 'python3')
-#INTERP = os.path.join(os.environ['HOME'], 'artishan.io', 'venv', 'bin', 'python3')
+#INTERP = os.path.join(os.environ['HOME'], 'opt', 'python-3.8.2', 'bin', 'python3')
+INTERP = os.path.join(os.environ['HOME'], 'artishan.io', 'venv', 'bin', 'python3')
 if sys.executable != INTERP:
         print("Adding Path")
         os.execl(INTERP, INTERP, *sys.argv)
-sys.path.append(os.getcwd(), 'myapp')
+sys.path.append(os.getcwd())
+sys.path.append(os.getcwd() + '/myapp')
 
 #sys.path.append('home/artishan.io/myapp')
 sys.path.append(os.path.join(os.environ['HOME'], 'artishan.io', 'myapp'))
@@ -14,6 +15,7 @@ sys.path.append(os.path.join(os.environ['HOME'], 'artishan.io', 'myapp'))
 sys.path.insert(0,'$HOME/opt/python-3.8.2/bin/python3')
 sys.path.insert(0,'$HOME/artishan.io/myapp')
 sys.path.insert(0,'$HOME/artishan.io/venv/bin')
+
 
 from myapp import app as application
  
